0a8fc24578e5096c6ac3d27765cdaf5925ce768e
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-04-17 19:10:57 +0600

    Made menu responsive

--- src/components/Header/CustomLink.jsx
+++ src/components/Header/CustomLink.jsx
@@ -1,12 +1,12 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-const CustomLink = ({ children, to, ...props }) => {
+const CustomLink = ({ children, to, className, ...props }) => {
   let resolved = useResolvedPath(to);
   let match = useMatch({ path: resolved.pathname, end: true });
 
   return (
     <Link
-      className={`text-green-400 hover:text-green-700 text-xl ${match ? 'text-green-700 font-bold' : 'font-semibold'}`}
+      className={`text-white md:text-green-400 hover:text-green-700 text-xl ${match ? 'text-green-300 md:text-green-600 font-bold' : 'font-semibold'} ${className}`}
       to={to}
       {...props}
     >
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../Layout/Container'
 import CustomLink from './CustomLink'
+import { BiMenu, BiX } from 'react-icons/bi'
 
 const Header = () => {
+  const [show, setShow] = useState(false)
+  const Icon = show ? BiX : BiMenu
+
   return (
     <div>
-        <Container className='py-3 flex items-center'>
+        <Container className='h-16 flex items-center relative'>
             <div>
                 <img src="/images/logo.png" alt="Wildie Logo" />
             </div>
 
-            <div className='flex gap-4 ml-auto'>
-                <CustomLink to="/">Home</CustomLink>
-                <CustomLink to="/about">About</CustomLink>
-                <CustomLink to="/contact">Conact Us</CustomLink>
+            <div 
+              className={`flex flex-col md:gap-4 md:flex-row ml-auto absolute 
+                          md:static top-16 left-0 z-50 bg-green-900 md:bg-transparent 
+                          w-full md:w-auto ${show ? '' : 'hidden md:flex'}`
+            }>
+                <CustomLink className='w-full py-2 px-3 md:px-0 md:py-0 md:w-auto' to="/">Home</CustomLink>
+                <CustomLink className='w-full py-2 px-3 md:px-0 md:py-0 md:w-auto' to="/about">About</CustomLink>
+                <CustomLink className='w-full py-2 px-3 md:px-0 md:py-0 md:w-auto' to="/contact">Conact Us</CustomLink>
+            </div>
+
+            <div className='ml-auto md:hidden' onClick={() => setShow(!show)}>
+              <Icon className='text-3xl text-green-900' />
             </div>
         </Container>
     </div>
