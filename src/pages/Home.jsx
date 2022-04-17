8086c12cf60d32655b068275e862807c4b972aff
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-04-17 19:57:32 +0600

    Show services section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../sections/Banner/Banner'
 import Main from '../components/Layout/Main'
+import Services from '../sections/Services/Services'
 
 const Home = () => {
   return (
     <Main>
       <Banner />
+      <Services />
     </Main>
   )
 }
