cd386bf03690a8d23f8ba49f58bd3a26f0c72dfc
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-04-17 20:09:50 +0600

    Added price and description

--- src/sections/Services/Service.jsx
+++ src/sections/Services/Service.jsx
@@ -7,7 +7,8 @@ const Service = ({ service }) => {
   return (
     <div className='p-4 bg-green-700 rounded-md relative sm:last:col-span-2 md:last:col-span-1'>
       <span className='absolute top-5 right-5 text-5xl'>{icon}</span>
-      <h1 className='text-5xl font-bold text-pink-100 mb-10'>{name}</h1>
+      <h1 className='text-5xl font-bold text-pink-100 mb-2'>{name}<span className='ml-2 text-yellow-200 text-lg'>${price}</span></h1>
+      <p className='text-green-200 mb-5'>{description}</p>
 
       <ul>
         {features.map((feature, index) => <li className='font-semibold text-green-300 flex  gap-1 items-center -ml-1' key={index}><BiCheck className='text-2xl' />{feature}</li>)}
