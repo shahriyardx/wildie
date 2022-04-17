8761c7cc4b60464f318dddbf9e3f5a928d36c74c
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-04-17 19:16:17 +0600

    Banner improvement

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,12 +7,12 @@ const Banner = () => {
     <div className='w-full py-10 md:py-0 md:h-[500px] relative bg-green-400'>
       <Container className='grid grid-cols-1 md:grid-cols-2 h-full'>
         <div className='h-full flex flex-col justify-center items-center md:items-start'>
-          <h1 className='text-6xl uppercase font-bold text-green-800'>Wildie</h1>
+          <h1 className='text-6xl font-bold text-green-800'>WiLDiE</h1>
           <p className='text-xl text-green-100 mt-2 text-center md:text-left'>
             Capture professional wild life photos to make your moments better. Want yours photos to stand out? Don't hesitate to contact us.
           </p>
           <div className='mt-2'>
-            <Button className='text-base bg-green-900 text-green-50 rounded-lg'>Contact Us</Button>
+            <Button className='text-base bg-green-800 hover:bg-green-900 uppercase text-green-50 rounded-lg'>Contact Us</Button>
           </div>
         </div>
 
